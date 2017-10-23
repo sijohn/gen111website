3e212c7714172d5b52f5621a056587dbafe7024d
ronic <ronicgeorge@gmail.com>
2017-10-23 18:22:35 +0200

    Removing the code

--- client/app/admin/product/product.fields.ts
+++
@@ -1,7 +0,0 @@
-export class product {
-    static fields: Array<any> = [
-        { field: 'image', dataType: 'product-image' },
-        { field: 'name', dataType: 'product-detail', id: '_id', slug: 'slug' },
-        { field: 'active', dataType: 'boolean' },
-    ];
-}
